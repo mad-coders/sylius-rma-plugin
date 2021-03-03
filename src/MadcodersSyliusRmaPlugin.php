76eba099e6b76e6f5c99ce4346c75f89f7730568
Piotr Lewandowski <p.lewandowski@madcoders.pl>
2021-03-03 11:24:23 +0100

    fix plugin class

--- src/MadcodersSyliusRmaPlugin.php
+++ src/MadcodersSyliusRmaPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Madcoders\SyliusRmaPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class MadcodersSyliusRmaPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
