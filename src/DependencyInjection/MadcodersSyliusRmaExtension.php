3e1f4867be92b64231fa264fbd6a03842791858a
Piotr Lewandowski <p.lewandowski@madcoders.pl>
2021-03-03 13:35:16 +0100

    refactor routing structure and fix extensions and services

--- src/DependencyInjection/MadcodersSyliusRmaExtension.php
+++ src/DependencyInjection/MadcodersSyliusRmaExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class MadcodersSyliusRmaPluginExtension extends Extension
+final class MadcodersSyliusRmaExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
